4d3465f1ea81a61ba4a2262d8348ae37cbd7f946
Essam <essamelsharkawy88@gmail.com>
2023-02-01 11:38:26 +0200

    t

--- Middlewares/AuthorizeRole.js
+++ Middlewares/AuthorizeRole.js
@@ -1,18 +1,13 @@
-const verifyusers =(...allowed)=>{
- return (req,res,next)=>{
-    let userRole =req.role;
-    if(!userRole)
-    return res.status(401).json({message:"not authorized"})
- 
-     let getAllowedRoles = [...allowed];
-    
-     const isvalid = getAllowedRoles.includes(userRole);
-     if(!isvalid)
-     return res.status(401).json({message:"not authorized"})
-     else
-     next();
+const verifyusers = (...allowed) => {
+  return (req, res, next) => {
+    let userRole = req.role;
+    if (!userRole) return res.status(401).json({ message: "not authorized" });
 
- }
+    let getAllowedRoles = [...allowed];
 
-}
-module.exports =verifyusers
+    const isvalid = getAllowedRoles.includes(userRole);
+    if (!isvalid) return res.status(401).json({ message: "not authorized" });
+    else next();
+  };
+};
+module.exports = verifyusers;
--- Routes/appointmentScheduler.js
+++ Routes/appointmentScheduler.js
@@ -2,14 +2,14 @@ const express = require("express");
 const controller = require("./../Controllers/appointmentScheduler");
 const teacherValidationArray = require("./../validation/appointmentScheduler");
 const router = express.Router();
-const whoIsValid = require("../Middlewares/AuthorizeRole")
+const whoIsValid = require("../Middlewares/AuthorizeRole");
 
 router
   .route("/appointmentScheduler")
-  .get(whoIsValid('employee'),controller.getAllAppointments)
-  .post(whoIsValid('employee'),controller.addAppointment)
-  .patch(whoIsValid('employee'),controller.editAppointment)
-  .delete(whoIsValid('employee'),controller.deleteAppointment);
+  .get(whoIsValid("employee"), controller.getAllAppointments)
+  .post(whoIsValid("employee"), controller.addAppointment)
+  .patch(whoIsValid("employee"), controller.editAppointment)
+  .delete(whoIsValid("employee"), controller.deleteAppointment)
 
   .get(controller.getAllAppointments)
   .post(controller.addAppointment)
