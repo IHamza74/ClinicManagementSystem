1d84bdf65cce4d5fd544133f5680e1c2bd9abdae
Essam <essamelsharkawy88@gmail.com>
2023-02-01 02:01:35 +0200

    prescription

--- Controllers/prescription.js
+++ Controllers/prescription.js
@@ -5,11 +5,11 @@ const PrescriptionSchema = mongoose.model("Prescriptions");
 //get All Prescription
 exports.getAllPrescriptions = (request, response, next) => {
   PrescriptionSchema.find()
-    .populate({
+    /* .populate({
       path: "appointmentId",
       select: { _id: 1, doctorID: 1, patientID: 1, clinicID: 1, employeeID: 1, date: 1 },
-    })
-    .populate({ path: "medicineID", select: { _id: 0, Name: 1, Dose: 1 } })
+    }) */
+    /* .populate({ path: "medicineID", select: { _id: 0, Name: 1, Dose: 1 } }) */
     .then((data) => {
       response.status(200).json({ data });
     })
@@ -19,11 +19,11 @@ exports.getAllPrescriptions = (request, response, next) => {
 //get Prescription By Id
 exports.getPrescriptionsById = (request, response, next) => {
   PrescriptionSchema.find({ _id: request.params.id })
-    .populate({
+    /* .populate({
       path: "appointmentId",
       select: { _id: 1, doctorID: 1, patientID: 1, clinicID: 1, employeeID: 1, date: 1 },
-    })
-    .populate({ path: "medicineID", select: { _id: 0, Name: 1, Dose: 1 } })
+    }) */
+    /* .populate({ path: "medicineID", select: { _id: 0, Name: 1, Dose: 1 } }) */
     .then((data) => {
       response.status(200).json({ data });
     })
--- Models/prescriptionModel.js
+++ Models/prescriptionModel.js
@@ -5,7 +5,12 @@ const medicineSchema = require("./medicineSchema");
 const prescriptionSchema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   medicine: [medicineSchema],
-  appointmentId: { type: Number, required: true, unique: true, ref: "appointmentScheduler" },
+  appointmentId: {
+    type: Number,
+    required: [true, "You must enter an appointment id for prescription"],
+    unique: true,
+    ref: "appointmentScheduler",
+  },
 });
 
 mongoose.model("Prescriptions", prescriptionSchema);
