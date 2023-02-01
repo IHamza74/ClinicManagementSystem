49b9e989c7bd28c8f55ed7c40883b01d70851787
Essam <essamelsharkawy88@gmail.com>
2023-02-01 02:12:59 +0200

    Prescription

--- Models/medicineSchema.js
+++ Models/medicineSchema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const medicineSchema = new mongoose.Schema(
   {
-    medicineID: { type: Number, ref: "medicines", required: true },
+    medicineID: { type: Number, ref: "Medicine", required: true },
     quantity: { type: Number, required: true },
   },
   { _id: false }
